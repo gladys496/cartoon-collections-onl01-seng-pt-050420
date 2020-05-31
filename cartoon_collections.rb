8f4e78b8ce5a224f94487368ef11d3403ccd41df
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-05-31 18:58:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name)
+  name.each_with_index do |name, index|
+    index += 1
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(captain)
+  captain.collect do |planet|
+    planet.capitalize << "!"
+  end 
 end
 
 def long_planeteer_calls# code an argument here
